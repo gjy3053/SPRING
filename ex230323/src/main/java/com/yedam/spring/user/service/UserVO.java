f8ce40c0247cf55523c9d515ae3bef8978a73242
Jiyeon <gjy30533@naver.com>
2023-03-24 11:16:34 +0900

    23.03.24

--- ex230323/src/main/java/com/yedam/spring/user/service/UserVO.java
+++ ex230323/src/main/java/com/yedam/spring/user/service/UserVO.java
@@ -10,8 +10,8 @@ import lombok.Data;
 public class UserVO {	
 	private String name;
 	private Integer age;
-	//java.util.Date -> 포맷이 우리나라 방식과 많이 다름
-	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	//java.util.Date -> 포맷이 우리나라 방식과 많이 다름 yyyy-MM-dd
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	//DateTimeFormat -> 입력받을 때 형식 정하는거, 출력과는 상관없다
 	private Date today;
 	
--- ex230323/src/main/java/com/yedam/spring/user/web/UserRestController.java
+++ ex230323/src/main/java/com/yedam/spring/user/web/UserRestController.java
@@ -1,10 +1,20 @@
 package com.yedam.spring.user.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yedam.spring.user.service.UserListVO;
 import com.yedam.spring.user.service.UserVO;
 
 @CrossOrigin
@@ -18,5 +28,61 @@ public class UserRestController {
 		System.out.println("age : " + userVO.getAge());
 		
 		return userVO; 
+	}
+	
+	@RequestMapping("/insertUsers")
+	public List<UserVO> insertUserList(UserListVO userListVO) {
+		for(UserVO user : userListVO.getList()) {
+			System.out.println(user);
+		}
+		return userListVO.getList();
+	}
+	
+	@RequestMapping("/getUserData")
+	public UserVO getUserData(@RequestParam("id") String name, @RequestParam(defaultValue = "0" , required=false) Integer age) {
+		UserVO userVO = new UserVO();
+		userVO.setName(name);
+		userVO.setAge(age);
+		
+		return userVO;
+	}
+	
+	@GetMapping("/getDataList") 
+	public String getDataList(@RequestParam(required = false) Map<String, Object> map) {
+		String message = null;
+		if(map.isEmpty()) {
+			message = "데이터가 존재하지 않습니다.";
+		}else {
+			message = "데이터가 생성되었습니다.";
+			Set<Map.Entry<String,Object>> entryList = map.entrySet();
+			for(Map.Entry<String, Object> entry:entryList) {
+				System.out.println("Key : " + entry.getKey());
+				System.out.println(",Value : " + entry.getValue());
+			}
+		}
+		return message;
+	}
+	
+	@RequestMapping("/getNames")
+	public Map<String, Object> getNames(@RequestParam List<String> name) {
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("result", "success");
+		map.put("data", name);
+		
+		return map;
+	}
+	
+	@RequestMapping("/getEmpInfo/{employeeId}")
+	public UserVO getEmpInfo(@PathVariable String employeeId) {
+		UserVO userVO = new UserVO();
+		userVO.setName(employeeId);
+		
+		return userVO;
+	}
+	
+	@RequestMapping("/getInfo")
+	public UserVO getInfo(@RequestBody UserVO userVO) {
+		return userVO;
 	}
 }
